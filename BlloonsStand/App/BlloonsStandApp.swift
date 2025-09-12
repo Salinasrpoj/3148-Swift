158472400cca6b640813574a4e5ecd1490809828
SBabchukTM <babchuk.serhii@trident-media.agency>
2025-09-12 13:49:35 +0300

    init

---
+++ BlloonsStand/App/BlloonsStandApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlloonsStandApp.swift
+//  BlloonsStand
+//
+//  Created by Serhii Babchuk on 12.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct BlloonsStandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BSLaunchView()
+        }
+    }
+}
